ffe27b13cfb2c507725a28a4fb9b8319abf94a6e
E2029-Aydin <aydinsanoz2@gmail.com>
2020-12-12 13:28:49 +0300

    localhost expression adden to app.listen

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ app.use(express.json())
 app.use('/api',router)
 
 app.listen(port, ()=>{
-    console.log(`I 'am listening on port ${port}`)
+    console.log(`I 'am listening on port http://localhost:${port}`)
 })
