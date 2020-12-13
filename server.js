dae8f94a75c107a7f54188236393cb4d9b8506a0
E2029-Aydin <aydinsanoz2@gmail.com>
2020-12-14 02:22:05 +0300

    react and serverSide connected

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
 const express = require("express")
+const app = express()
+
 require("dotenv").config()
+
+
 const connectDB = require("./model/connectDB")
 const router = require("./routes/router")
 
 
-
-const app = express()
-const port = process.env.PORT
 connectDB()
 
-// responds to /api router
 
+//app.use(cors()) //to pass cors policy localhost:3000 to localhost:5000 in mern project
+
+// responds to /api router
 app.use(express.json())
 app.use('/api',router)
 
+
+
+// Production Check
+if(process.env.NODE_ENV==="production") {
+    app.use(express.static("/client/build"))
+    app.get("*", (req,res)=>{
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+        
+    });
+}
+
+const port = process.env.PORT || 5000
 app.listen(port, ()=>{
     console.log(`I 'am listening on port http://localhost:${port}`)
 })
